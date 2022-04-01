62673e466f50c6beb63bc550e5c281b43d594150
StevenC623 <77308361+StevenC623@users.noreply.github.com>
2022-04-01 13:42:33 -0700

    Update Cluster.R

--- Cluster.R
+++ Cluster.R
@@ -1,5 +1,6 @@
 library(tidyverse)
 library(cluster)
+library(dbscan)
 library(ggpubr)
 
 # import cleaned data -----------------------------------------------------
@@ -7,6 +8,7 @@ library(ggpubr)
 df_sample <- read.csv('hmda_2017_nevada_cleaned.csv',stringsAsFactors = T)
 df_sample$action_taken_name <- as.factor(df_sample$action_taken_name)
 
+str(df_sample)
 summary(df_sample)
 # remove 'action_taken' for now -------------------------------------------
 
@@ -19,14 +21,27 @@ df_num <- df_sample %>% select_if(is.numeric)
 
 mdist <- mahalanobis(df_num, colMeans(df_num), cov(df_num))
 pval <- pchisq(mdist, df=ncol(df_num)-1, lower.tail=FALSE)
-alpha <- 0.05
-sum(pval < alpha) #5634 outliers
+alpha <- 0.01
+sum(pval < alpha) 
 
-df_num_noout <- df_num_noout[which(pval >= alpha),]
+df_num_noout <- df_num[which(pval >= alpha),]
 
-# optimal k ----------------------------------------------------------
 gd <- daisy(df_main, metric = 'gower')
+min(gd)
+median(gd)
+max(gd)
 
+# DBSCAN outliers ----------------------------------------------------------
+db <- dbscan(gd, eps=0.12, minPts=50)
+print(db)
+
+table(db = as.factor(db$cluster), md = as.factor(ifelse(pval < alpha, 0, 1)))
+
+# Describe characteristics of outliers
+
+
+# Partition Clustering ------------------------------------------------
+# optimal k  for Kmean/Kmedoid---------
 #sillouette score
 sw<-function(km, d)
 {
@@ -135,6 +150,21 @@ for (i in 1:(ncol(df_km_eu)-1)){
 
 ggarrange(plotlist=plot_listnum)
 
+# Hierarchy Clustering
+hc <- hclust(gd, method = "ward.D2")
+sw_list <- numeric()
+
+for (i in 2:6){
+  sw <- silhouette(cutree(hc, k = i), gd)
+  sw_list <- c(sw_list, mean(sw[,3]))}
+
+sw_list
+which.max(sw_list)+1
+
+table(cutree(hc, k = 3)) 
+rect.hclust(hc, k = 3, border = 2:4)
+
+# plot characteristics 
 
 # compare with action taken -----------------------------------------------
 km5 <- kmeans(gd,2)
